5003f788b9ec5a2544fcb0776a92ad6cf2e4f7ab
elywin <lukeeraelywin@gmail.com>
2021-08-31 01:06:02 +0300

    test endpoint

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,16 @@
 import test from 'japa'
+import { JSDOM } from 'jsdom'
+import supertest from 'supertest'
 
-test.group('Example', () => {
-  test('assert sum', (assert) => {
-    assert.equal(2 + 2, 4)
-  })
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Welcome', () => {
+  test('ensure home page works', async (assert) => {
+    /**
+     * Make request
+     */
+    const  text  = await supertest(BASE_URL).get('/').expect(200)
+    console.log(text)
+    assert.equal(text.text, '{"hello":"world"}')
+})
 })
